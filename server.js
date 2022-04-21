5074413871422b5e0c635403735c235fb11636b6
JuaneProd <juaneparrilla@gmail.com>
2022-04-21 13:12:38 +0200

    Widget 2 Test

--- server.js
+++ server.js
@@ -11,72 +11,70 @@ app.set("view engine", "ejs");
 app.use(express.json());
 
 let observers = {
-  zer0x: {
-    url: "",
-    key: "12341234",
-  },
-  Juane: {
-    url: "",
-    key: "1234",
-  }
+    zer0x: {
+        url: "",
+        key: "12341234",
+    },
+    Juane: {
+        url: "",
+        key: "1234",
+    }
 };
 
 app.get("/widget/:observer", (req, res) => {
-  res.render("widget", {
-    urlWidget: observers[req.params.observer],
-    observer: req.params.observer,
-  });
+    res.render("widget", {
+        urlWidget: observers[req.params.observer],
+        observer: req.params.observer,
+    });
 });
 
+
 app.post("/", (req, res) => {
-  console.log("/: ", req.body);
+    console.log("/: ", req.body);
 });
 
 app.get("/urlWidget/:observer", (req, res) => {
-  res.send(observers[req.params.observer].url);
+    res.send(observers[req.params.observer].url);
 });
 
-app.post("/urlWidget/:observer", async (req, res) => {
-  if (req.body.key === observers[req.params.observer].key) {
-    observers[req.params.observer].url = req.body.url;
+app.post("/urlWidget/:observer", async(req, res) => {
+    if (req.body.key === observers[req.params.observer].key) {
+        observers[req.params.observer].url = req.body.url;
 
-    const headers = {
-      headers: { "TRN-Api-Key": "d750f397-264f-4259-a8ac-f38d984aefaf" },
-    };
+        const headers = {
+            headers: { "TRN-Api-Key": "d750f397-264f-4259-a8ac-f38d984aefaf" },
+        };
 
-    console.log(req.body.player);
-    var playerName = req.body.player;
-    console.log(playerName);
+        console.log(req.body.player);
+        var playerName = req.body.player;
+        console.log(playerName);
 
-    const player = await axios
-      .get(
-        `https://api.fortnitetracker.com/v1/profile/pc/${playerName}`,
-        headers
-      )
-      .catch((err) => {
-        observers[req.params.observer].url =
-          "https://trackercdn.com/cdn/fortnitetracker.com/icons/flags/globe.png";
-        res.status(400).send("Unvalid player");
-      });
+        const player = await axios
+            .get(
+                `https://api.fortnitetracker.com/v1/profile/pc/${playerName}`,
+                headers
+            )
+            .catch((err) => {
+                observers[req.params.observer].url =
+                    "https://trackercdn.com/cdn/fortnitetracker.com/icons/flags/globe.png";
+                res.status(400).send("Unvalid player");
+            });
 
-    if (player.data.country) {
-      observers[
-        req.params.observer
-      ].url = `https://cdn.thetrackernetwork.com/cdn/flags/4x3/${player.data.country}.svg`;
+        if (player.data.country) {
+            observers[
+                req.params.observer
+            ].url = `https://cdn.thetrackernetwork.com/cdn/flags/4x3/${player.data.country}.svg`;
 
-      res.status(200).send(observers[req.params.observer].url);
+            res.status(200).send(observers[req.params.observer].url);
+        } else {
+            observers[req.params.observer].url =
+                "https://trackercdn.com/cdn/fortnitetracker.com/icons/flags/globe.png";
+
+            res.status(201).send(observers[req.params.observer].url);
+        }
     } else {
-      observers[req.params.observer].url =
-        "https://trackercdn.com/cdn/fortnitetracker.com/icons/flags/globe.png";
-
-      res.status(201).send(observers[req.params.observer].url);
+        res.status(404).send("Invalid credentials!");
     }
-  } else {
-    res.status(404).send("Invalid credentials!");
-  }
 });
-
-const PORT = 8080;
-const HOST = "localhost";
 
 app.listen(process.env.PORT || 8080);
